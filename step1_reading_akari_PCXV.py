6bae33d150141cec03c6af8666e9761ab61b0224
Aaron C. Bell <aaroncnb@users.noreply.github.com>
2015-06-22 18:38:30 +0900

    Update step1_reading_akari_PCXV.py
    Replaced IDL comment symbol ; with python comment symbol #

--- step1_reading_akari_PCXV.py
+++ step1_reading_akari_PCXV.py
@@ -1,25 +1,25 @@
-;PRO step1_reading_akari_
-;;*****************************************************************************
-;;*
-;;*          International Young Astronomer School on Exploiting
-;;*                   Herschel and Planck Data (2013)
-;;*
-;;*               STEP1: Reading and visualizing the data
-;;*                       (Created by F. Galliano, edited in an exceedingly crude way by A. Bell)
-;;*
-;;*****************************************************************************
 
-     ;; 0) Specify the region you want to study (Need this step since we have access to all-sky data)
-     ;; Manually specifiying the ROI name and the Galactic coordinates. 
-     ;; I would like to modify this step so that the coordinates are extracted from the ROI name, 
-     ;; and I want the ROI to be given as a parameter when the "step1..." procedure is called.
-     ;; I would also like to implement a step7, which will procedurally generate a thesis
-;;;Let's import the healpix and numpy packages needed for this analysis...
+##*****************************************************************************
+##*
+##*          International Young Astronomer School on Exploiting
+##*                   Herschel and Planck Data (2013)
+##*
+##*               STEP1: Reading and visualizing the data
+##*                       (Created by F. Galliano, edited in an exceedingly crude way by A. Bell)
+##*
+##*****************************************************************************
+
+     ## 0) Specify the region you want to study (Need this step since we have access to all-sky data)
+     ## Manually specifiying the ROI name and the Galactic coordinates. 
+     ## I would like to modify this step so that the coordinates are extracted from the ROI name, 
+     ## and I want the ROI to be given as a parameter when the "step1..." procedure is called.
+     ## I would also like to implement a step7, which will procedurally generate a thesis
+###Let's import the healpix and numpy packages needed for this analysis...
 
 import healpy as hp
 import numpy as np
 
-;;;;;;;;0.2) Read in the HEALPix Maps Before starting the LOOP OF ALL REGION
+########0.2) Read in the HEALPix Maps Before starting the LOOP OF ALL REGION
 print "Reading HEALPix Maps"
        map12 = hp.read_map("../Data/im_iras12.fits")
        map25 = hp.read_map("../Data/im_iras25.fits")
@@ -38,24 +38,24 @@ FOR cerberus=0,NROIs-1 DO BEGIN
 
 ROI  = AME.field01[cerberus]
 
-;;There seems to be a problem with converting the ROI name into GLON and GLAT. It's writes them into the header as a string...
-;;Somehow GNOMDRIZZ can interpret the string, but later on HASTROM (in step3) cannot
-;glon = STRMID(ROI, 1,6)
-;glat = STRMID(ROI,7)
+##There seems to be a problem with converting the ROI name into GLON and GLAT. It's writes them into the header as a string...
+##Somehow GNOMDRIZZ can interpret the string, but later on HASTROM (in step3) cannot
+#glon = STRMID(ROI, 1,6)
+#glat = STRMID(ROI,7)
 glon = AME.field02[cerberus]
 glat = AME.field03[cerberus]
 
 
 
-;; List of the broadbands to be used (Just throwing in the AKARI bands here)
-;;----------------------------------
-;; This array contains the string label of each of the waveband we
-;; want to  analyze.
-;; Currently testing with only the FIS and HFI bands- all of the ones available as HEAPIX.
-;; Adding IRC bands will need IRC HEALPIX files, 
-;;;;;;;or some parallel process which gets the data via the "ircscan" bash script by Ohsawa-san
+## List of the broadbands to be used (Just throwing in the AKARI bands here)
+##----------------------------------
+## This array contains the string label of each of the waveband we
+## want to  analyze.
+## Currently testing with only the FIS and HFI bands- all of the ones available as HEAPIX.
+## Adding IRC bands will need IRC HEALPIX files, 
+#######or some parallel process which gets the data via the "ircscan" bash script by Ohsawa-san
 
-;bands = ["akari9","iras12","akari18","iras25","iras60","akari65","akari90","iras100","akari140","akari160","planck857","planck545"]
+#bands = ["akari9","iras12","akari18","iras25","iras60","akari65","akari90","iras100","akari140","akari160","planck857","planck545"]
 
 bands =["iras12","iras25","iras60","akari65","akari90","iras100","akari140","akari160","planck857","planck545"]
 
@@ -64,40 +64,40 @@ Nband = N_ELEMENTS(bands)
 FOR i=0,Nband-1 DO BEGIN
 
 
-  ;; 1) Read the FITS file 
-  ;; Now that HEALPIX FITS files are available for FIS as well as HFI data, I will modify this step to extract cutouts from HPY maps
-  ;;----------------------
-;  PRINT, "Reading the map "+bands[i]+"..."+ROI+"..."+glon+"..."+glat
+  ## 1) Read the FITS file 
+  ## Now that HEALPIX FITS files are available for FIS as well as HFI data, I will modify this step to extract cutouts from HPY maps
+  ##----------------------
+#  PRINT, "Reading the map "+bands[i]+"..."+ROI+"..."+glon+"..."+glat
 
 
-;order 13 pixel spacing in degrees is 0.0071625
-;0.42975 in arcminutes...
-;     reso = 0.42975 ELSE (Order 13pr)
-;;;ORDER 13 is too pixely, and IDL chokes on it :( compromising for now with ORDER 12
-;order 12 pixel spacing is 0.014325 degrees
-;
+#order 13 pixel spacing in degrees is 0.0071625
+#0.42975 in arcminutes...
+#     reso = 0.42975 ELSE (Order 13pr)
+###ORDER 13 is too pixely, and IDL chokes on it :( compromising for now with ORDER 12
+#order 12 pixel spacing is 0.014325 degrees
+#
 reso1 = 1.7
-;reso2 = 0.8595
+#reso2 = 0.8595
 size_arcmin = 240
 smoothing_size_arcmin = 60
-;; Read-in each band's data using the appropriate routine for each (i.e. gnomdrizz for HEALPix, simple READFITS for non HEALPix)
-;; The procedure READ_FITS_MAP is part of the HEALPiX IDL library.
-;; GNOMDRIZZ is from the DRIZZLIB package (And is a very silly-sounding routine name)
+## Read-in each band's data using the appropriate routine for each (i.e. gnomdrizz for HEALPix, simple READFITS for non HEALPix)
+## The procedure READ_FITS_MAP is part of the HEALPiX IDL library.
+## GNOMDRIZZ is from the DRIZZLIB package (And is a very silly-sounding routine name)
 
     CASE bands[i] OF
        'akari9':BEGIN
        img = readfits("../Data/akari9/"+ROI+"_"+bands[i]+".fits",hdr)
        img[where(img[*] eq 0.00000)] = !Values.F_NAN       
-       ;img = img*0.31       
-       ;backsub, img
-       ;img = FILTER_IMAGE(img, FWHM=  120, /ALL_PIXELS)
+       #img = img*0.31       
+       #backsub, img
+       #img = FILTER_IMAGE(img, FWHM=  120, /ALL_PIXELS)
     END
        'akari18':BEGIN
        img = readfits("../Data/akari18/"+ROI+"_"+bands[i]+".fits",hdr)
        img[where(img[*] eq 0.00000)] = !Values.F_NAN
        img = img*0.48
-       ;backsub, img
-       ;img = FILTER_IMAGE(img, FWHM=  120, /ALL_PIXELS)
+       #backsub, img
+       #img = FILTER_IMAGE(img, FWHM=  120, /ALL_PIXELS)
     END
       'iras12':BEGIN
        gnomdrizz,hparrays.map12[*,0],rot=[glon,glat],reso_arcmin=reso1,pxsize=ROUND(size_arcmin/reso1),mapfits=img,hfits=hdr, /nested
@@ -148,64 +148,64 @@ ENDCASE
 
 
 
-  ;;  - Extension 1: RMS in MJy/sr.
-  ;; In the case of IRC, just make an array with the same dimension as the image. Then set all values to 10% of the signal
+  ##  - Extension 1: RMS in MJy/sr.
+  ## In the case of IRC, just make an array with the same dimension as the image. Then set all values to 10% of the signal
   
 rms = img*0.1D
-;rms_total = 
+#rms_total = 
 
-  ;; 1.1) Background Subtraction: Let's try to do a background subtraction inside of this step.
-  ;; I think "SKY" is too crude, so I will instead use "BACKSUB" from the Buie library...
-  ;;.... It fits a linear or polynomial background to each column or each row of the image, then subtracts it
-  ;; The default method is to subtract a mean.
+  ## 1.1) Background Subtraction: Let's try to do a background subtraction inside of this step.
+  ## I think "SKY" is too crude, so I will instead use "BACKSUB" from the Buie library...
+  ##.... It fits a linear or polynomial background to each column or each row of the image, then subtracts it
+  ## The default method is to subtract a mean.
 
-;backsub,img
+#backsub,img
 
 
 
 writefits, ROI+bands[i]+'.fits',img, hdr
 
-   ;;This one can fit a plane or 2-d polynomial to the whole image...which is best?
-   ;; I think that if the zody is the dominant background, a plane is best.
-   ;;    Continnues to have some issue with the "LOWESS" routine
-   ;skyfit,img,skyimg,YORDER=0,XORDER=0, NDEG=1
-   ;img = img-skyimg
+   ##This one can fit a plane or 2-d polynomial to the whole image...which is best?
+   ## I think that if the zody is the dominant background, a plane is best.
+   ##    Continnues to have some issue with the "LOWESS" routine
+   #skyfit,img,skyimg,YORDER=0,XORDER=0, NDEG=1
+   #img = img-skyimg
 
 
-  ;; Size of the image
+  ## Size of the image
   sizim = SIZE(img)
   Nx = sizim[1]
   Ny = sizim[2]
 
 
-  ;; 2) Make a mask
-  ;;---------------
-  ;; Sometimes, pixels are NaN (Not a Number), either because they
-  ;; have been masked out during the data processing, or they are simply off
-  ;; the field. Here we build a mask for each map. This mask will be 0 if 
-  ;; the pixel is OK and 1 otherwise.
+  ## 2) Make a mask
+  ##---------------
+  ## Sometimes, pixels are NaN (Not a Number), either because they
+  ## have been masked out during the data processing, or they are simply off
+  ## the field. Here we build a mask for each map. This mask will be 0 if 
+  ## the pixel is OK and 1 otherwise.
 
   mask = BYTARR(Nx,Ny)
   whbad = WHERE((img LE 0.D) AND (FINITE(img) EQ 0), Nbad)
   IF (Nbad GT 0) THEN mask[whbad] = 1
 
 
-  ;; 3) Statistics on the data
-  ;;--------------------------
-  ;; Signal-to-noise keeping only the NaNaNs (Not a NaN).
+  ## 3) Statistics on the data
+  ##--------------------------
+  ## Signal-to-noise keeping only the NaNaNs (Not a NaN).
     SovN = (ABS(img/rms))[WHERE(mask EQ 0)]
     PRINT, "  The signal-to-noise ratio for the map is on average " $
          + STRTRIM(MEAN(SovN),2)+" between "+STRTRIM(MIN(SovN),2)+" and " $
           + STRTRIM(MAX(SovN),2)+"."
 
-  ;; In all these programs, we use the STRTRIM(var,2) formatting
-  ;; command, that converts a number to a character string and removes
-  ;; the extra spaces before and after.
+  ## In all these programs, we use the STRTRIM(var,2) formatting
+  ## command, that converts a number to a character string and removes
+  ## the extra spaces before and after.
 
 
-  ;; 4) Plotting the image
-  ;;----------------------
-  ;; Plot the image, with a reduce size.
+  ## 4) Plotting the image
+  ##----------------------
+  ## Plot the image, with a reduce size.
 
   maxsize = 500.
   reduction = maxsize/MAX([Nx,Ny])
@@ -216,32 +216,32 @@ writefits, ROI+bands[i]+'.fits',img, hdr
   LOADCT, 15
   TVSCL, CONGRID(img*(1-mask),Nx_plot,Ny_plot)
 
-  ;CONTOUR, DIST(31,41), POSITION=[0.15, 0.15, 0.95, 0.75], $
-  ;C_COLORS=INDGEN(25)*4, NLEVELS=25
+  #CONTOUR, DIST(31,41), POSITION=[0.15, 0.15, 0.95, 0.75], $
+  #C_COLORS=INDGEN(25)*4, NLEVELS=25
   
-  ;; The IDL CONGRID function interpolates an array on a new
-  ;; grid. It has to be handled carefully when performing photometry
-  ;; because of possible interpolation errors. However, for display
-  ;; purposes, as in this case, it works fine.
+  ## The IDL CONGRID function interpolates an array on a new
+  ## grid. It has to be handled carefully when performing photometry
+  ## because of possible interpolation errors. However, for display
+  ## purposes, as in this case, it works fine.
 
 
-  ;; 5) Savings
-  ;;-----------
-  ;; We write one IDL save file per waveband.
+  ## 5) Savings
+  ##-----------
+  ## We write one IDL save file per waveband.
   filexdr = "../Save/step1_"+bands[i]+"_"+ROI+".xdr"
   SAVE, FILE=filexdr, Nx, Ny, img, mask, rms, hdr
-;  SAVE, FILE=filexdr, Nx, Ny, img, mask, hdr
+#  SAVE, FILE=filexdr, Nx, Ny, img, mask, hdr
   PRINT, " - File "+filexdr+" has been written."
-    ;;5b) We need to clear the 'hdr' variable, since GNOMDRIZZ will use it as input header if it's already defined, i.e. by READFITS  
-    ;;  Using "UNDEFINE" as suggested by CoyoteIDL, since "DELVAR" deletes at the main level and causes problems for reasons.
+    ##5b) We need to clear the 'hdr' variable, since GNOMDRIZZ will use it as input header if it's already defined, i.e. by READFITS  
+    ##  Using "UNDEFINE" as suggested by CoyoteIDL, since "DELVAR" deletes at the main level and causes problems for reasons.
      UNDEFINE,hdr
       
 
   PRINT
 ENDFOR
 
-;; 6) Save a general file
-;;-----------------------
+## 6) Save a general file
+##-----------------------
 filexdr = "../Save/step1_general_.xdr"
 SAVE, FILE=filexdr, bands, Nband
 PRINT, " - File "+filexdr+" has been written."
