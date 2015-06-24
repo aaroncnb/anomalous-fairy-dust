07a953890bc0cee3c5222a495cbf837f2cfb490b
Aaron C. Bell <aaroncnb@users.noreply.github.com>
2015-06-24 15:37:28 +0900

    Create step0_settings.py
    Migrated the basic image processing parameters from the beginning of step1 to this new "settings file": Smoothing resolution, image size, image pixel scale, and bands to be included in the analysis.

---
+++ step0_settings.py
@@ -0,0 +1,13 @@
+
+# Set which photometric bands' data are to be used in the analysis
+bands = ["iras12","iras25","iras60","akari65","akari90","iras100","akari140","akari160","planck857","planck545"]
+# Specify the pixel scale of the input data
+
+reso1 = 1.7
+
+#Specify the width of the desired cutouts, in pixels
+
+size_arcmin = 240
+
+# Set the smoothing width (in pixels) for the Guassian PSF Smoothing
+smoothing_size_arcmin = 60
