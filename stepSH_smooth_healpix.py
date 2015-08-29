2733c565acffc33f1170e494e5abb9850af629a3
Aaron C. Bell <aaroncnb@users.noreply.github.com>
2015-08-29 18:11:06 +0900

    Update stepSH_smooth_healpix.py
    Starting to write the code with a loop structure.
      However for the present it may be more efficient just to run the smothing line by line, map by map. I'll leave this code here for future reference.

--- stepSH_smooth_healpix.py
+++ stepSH_smooth_healpix.py
@@ -10,7 +10,8 @@ fwhm =  0.0174532925 # Should be given in radians!
 ########0.1) Read in the HEALPix Maps Before starting the LOOP OF ALL REGIONS
 
 print "Reading HEALPix Maps"
-
+maps = ["im_iras12","im_iras25,"im_iras60","im_akari65","im_akari90","im_iras100","im_akari140","im_akari160","im_planck857","im_planck545"]
+for i in range
 map12 = hp.read_map("../Data/im_iras12.fits", nest = True)
 map25 = hp.read_map("../Data/im_iras25.fits", nest = True)
 map60 = hp.read_map("../Data/im_iras60.fits", nest = True)
@@ -27,11 +28,13 @@ print "Finished reading HEALPix Maps"
 ############# http://healpy.readthedocs.org/en/latest/generated/healpy.sphtfunc.smoothing.html#healpy.sphtfunc.smoothing
 print "Smoothing HEALPix maps to" FWHM
 
-map_in = map12 
-map_out = healpy.sphtfunc.smoothing(map_in, fwhm)
-map12_smooth = map_out
+for i in range(0,nmaps):
+  map_in = maps[i]
+  map_out = healpy.sphtfunc.smoothing(map_in, fwhm)
 
 ###### 2.0) Now that the smoothing is finished, we'll write the smoothed maps to a new file:
 
-hp.write_map("im_iras12.fits", map12_smooth)
+hp.write_map(images[i]"_smoothed.fits", map_out)
+
+print "Smoothed" images[i] "to FWHM" fwhm "radians"
 
