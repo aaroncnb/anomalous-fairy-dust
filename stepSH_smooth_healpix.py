3f51240365e2dafe1948e18734b053dc67533262
Aaron C. Bell <aaroncnb@users.noreply.github.com>
2015-08-30 14:51:18 +0900

    Update stepSH_smooth_healpix.py
    Allowed the smoothing command to use arcmin instead of radians to specify the FWHM.

--- stepSH_smooth_healpix.py
+++ stepSH_smooth_healpix.py
@@ -5,22 +5,14 @@ im
 import numpy as np
 import healpy as hp
 
-fwhm =  0.0174532925 # Should be given in radians!
+fwhm =  60 # Should be given in radians!
 
-########0.1) Read in the HEALPix Maps Before starting the LOOP OF ALL REGIONS
+##### 0.1) Read in the HEALPix Maps Before starting the LOOP OF ALL REGIONS
 
 print "Reading HEALPix Maps"
 maps = ["im_iras12","im_iras25,"im_iras60","im_akari65","im_akari90","im_iras100","im_akari140","im_akari160","im_planck857","im_planck545"]
-for i in range
-map12 = hp.read_map("../Data/im_iras12.fits", nest = True)
-map25 = hp.read_map("../Data/im_iras25.fits", nest = True)
-map60 = hp.read_map("../Data/im_iras60.fits", nest = True)
-map100 = hp.read_map("../Data/healpix10/im_akari65.fits", nest = True)
-map90 = hp.read_map("../Data/healpix10/im_akari90.fits", nest = True)
-map140 = hp.read_map("../Data/healpix10/im_akari140.fits", nest = True)
-map160 = hp.read_map("../Data/healpix10/im_akari160.fits", nest = True)
-map857 = hp.read_map("../Data/im_planck857.fits", nest = True)
-map545 = hp.read_map("../Data/im_planck545.fits", nest = True)
+for i in range (0,Nmaps)
+  map_in = hp.read_map(maps[i]".fits", nest = True)
 
 print "Finished reading HEALPix Map" maps[i]
 
@@ -28,7 +20,7 @@ print "Masking bad pixels"
 
 ###### 0.2) We'll need to mask the bad pixels before feeding the maps to the visualization and smoothing routines
 
-hp.pixelfunc.ma(map_in, badval=None, rtol=1e-05, atol=1e-08, copy=True)
+hp.pixelfunc.ma(map_in, badval=None, rtol=1e-05, atol=1e-08, copy=False)
 
 ###### 0.3) Take a quick look at the map before smoothing it:
 
@@ -42,7 +34,7 @@ print "Smoothing HEALPix maps to" FWHM
 
 for i in range(0,nmaps):
   map_in = maps[i]
-  map_out = hp.sphtfunc.smoothing(map_in, fwhm)
+  map_out = hp.sphtfunc.smoothing(map_in, fwhm = fwhm, arcmin = True)
 
 ###### 1.1) Take a look at the map after smoothing it:
 hp.mollview(map_out, title='Histogram equalized Galactic, Smoothed', unit='MJy/sr', norm='hist', min=0,max=2000, xsize=2000)
